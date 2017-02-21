0582fe137f27f9dbedd6b07544924d79327fa3e6
David Baker <dbaker@inghamisd.org>
2017-02-21 08:55:18 -0500

    adding files for first commit

---
+++ Uno1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create a game similar to Uno: use the below as guidance
+            //removed wilds to change to suit of card to make code simpler feel free to change to suport full wild behaivior.
+            //Ace is Draw 4 Card can be played at any time and will switch suit to the suit of the Ace
+            //king is Wild card that can be played at any time and will switch suit to the suit of the King
+            //Queen is Draw 2 Card Next player draws 2 cards
+            //Jack is skip Card
+
+            ///////// The below is just for guidance - you do not need to do it the way outlined below/////////////
+
+            //create a list of players 
+            //ask the user to enter a number of players 2-5
+            //create as many players as indicated by user and add each to  the list of players
+            //create an object called gameDeck that is an instance of the deck class
+            //shuffle the game deck
+            //deal 7 cards to each player 
+            //draw the first card
+            //place card on the discard pile
+            //While the game is not over (players still have cards
+                // check if previous card was special card(see above) and do what it tell you to do
+                // check currentPlayer's available moves
+                // if no moves are available draw a card until a move is available
+                // if valid move found prompt user to enter cardName
+                // play the card the user selected and discard the card.
+                // move to next player in the list
+
+            
+                
+            
+        }
+    }
+}
